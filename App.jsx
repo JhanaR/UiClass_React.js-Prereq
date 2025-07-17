36aed9d40c2eb429f883ccea0f238dbf5b71ea1c
JhanaR <132604023+JhanaR@users.noreply.github.com>
2025-07-17 11:29:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+// import Nav from "./components/nav/Nav";
+
+// import Counter from "./components/counter/Counter";
+import Page from "./assignment/Page";
+function App() {
+  return (
+    <div>
+      {/* <Nav /> */}
+      {/* <Counter /> */}
+      <Page />
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment/Page.jsx
@@ -0,0 +1,49 @@
+import "./Style.css";
+
+function Page() {
+  return (
+    <>
+      <div className="card">
+          <img
+            src="https://i.pinimg.com/originals/f1/b4/f3/f1b4f35c609fd65e1012fe5c1e81ba9b.jpg"
+            width="200"
+            height="200"
+          />
+          <h3>Raj Verma</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit,
+            laboriosam.
+          </p>
+          <button>Profile Details</button>
+        </div>
+        <div className="card1">
+          <img
+            src="https://i.pinimg.com/originals/f1/b4/f3/f1b4f35c609fd65e1012fe5c1e81ba9b.jpg"
+            width="200"
+            height="200"
+          />
+          <h3>Sukumar Verma</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit,
+            laboriosam.
+          </p>
+          <button>Profile Details</button>
+        </div>
+        <div className="card2">
+          <img
+            src="https://i.pinimg.com/originals/f1/b4/f3/f1b4f35c609fd65e1012fe5c1e81ba9b.jpg"
+            width="200"
+            height="200"
+          />
+          <h3>Lehar Verma</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit,
+            laboriosam.
+          </p>
+          <button>Profile Details</button>
+        </div>
+    </>
+  );
+}
+
+export default Page;
